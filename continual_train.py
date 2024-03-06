21b4b2c7e4eca2f3050321b61b768af3bf58c091
Aref Azizpour <aref.azizpour@gmail.com>
2024-03-06 16:44:46 -0500

    continual_train.py stable version

--- continual_train.py
+++ continual_train.py
@@ -1,72 +1,116 @@
 from train import train
 from test import run_test as test
-from configs import configs_ft, continual_configs as configs
+from configs import ft_configs, continual_configs as cl_configs
 from utils.continual_utils import fill_configs_with_datasets
 
+
 if __name__ == "__main__":
-    configs_ft["Model"]["expert_ckpt"] = configs["Model"]["baseline_ckpt"]
-    configs_ft["Model"]["src_ckpts"].append(configs["Model"]["baseline_ckpt"])
-    fine_tuned_model_paths = []
+    ft_configs["Model"]["expert_ckpt"] = cl_configs["Model"]["ft_ckpt_paths"][0]
+    ft_configs["Model"]["src_ckpts"].append(cl_configs["Model"]["ft_ckpt_paths"][0])
+    ft_configs["Model"]["fine_tune"] = True
+    ft_configs["Model"]["model_type"] = "expert"
+    ft_configs["Train"]["epochs"] = cl_configs["Train"]["epochs"]
+    ft_configs["Train"]["lr"] = cl_configs["Train"]["lr"]
+    ft_configs["General"]["check_val_every_n_epoch"] = cl_configs["General"][
+        "check_val_every_n_epoch"
+    ]
+    assert (
+        ft_configs["General"]["check_val_every_n_epoch"]
+        <= ft_configs["Train"]["epochs"]
+    ), "Check val every n epoch should be less than or equal to the number of epochs"
     acc_matrix = []
     auc_matrix = []
-    seen_datasets = []
+    seen_datasets = [cl_configs["Data"]["synthetic_dataset_names"][0]]
 
-    for i, dataset in enumerate(configs["Data"]["synthetic_dataset_names"]):
-        task_id = i + 1 # the first task is the baseline therefore we skip it
+    for i, dataset in enumerate(cl_configs["Data"]["synthetic_dataset_names"]):
+        ft_configs["Model"]["fine_tune"] = True
+        ft_configs["Model"]["model_type"] = "expert"
+        ft_configs["Train"]["distill"] = cl_configs["Train"]["distill"] if i > 1 else False
         acc_matrix.append([])
         auc_matrix.append([])
-
-        seen_datasets.append(dataset)
-        configs_ft["Model"]["fine_tune"] = True
-        configs_ft["Model"]["model_type"] = "expert"
-        configs_ft = fill_configs_with_datasets(configs_ft, [dataset], configs["Data"]["real_dataset_name"])
-
-        print(f"Fine-tuning for dataset: {dataset}...")
-        model_checkpoint_state_dict = train(configs_ft)
-        print(f"Finished fine-tuning for dataset {dataset}")
-
-        last_model_path = model_checkpoint_state_dict["last_model_path"]
-        fine_tuned_model_paths.append(last_model_path)
-        configs_ft["Model"]["model_type"] = "moe"
-        configs_ft["Model"]["fine_tune"] = False
-        configs_ft["Model"]["src_ckpts"] = fine_tuned_model_paths
-
-        # add gan dataset to the list of datasets for training and testing the MOE
-        gan_included_datasets = ["dn-gan-500"] + seen_datasets
-        configs_ft = fill_configs_with_datasets(configs_ft, gan_included_datasets , configs["Data"]["real_dataset_name"])
         
-        if configs["Data"]["fixed_memory"]:
-            configs_ft["Data"]["train_dataset_limit_per_class"] = configs["Data"]["memory_size"] // 2 // (task_id + 1)
-            print(f"Memory size: {configs['Data']['memory_size']}")
-            print(f"Train dataset limit per class: {configs_ft['Data']['train_dataset_limit_per_class']}")
-            exit()
-        else:
-            loss_weights = [float(len(gan_included_datasets)), 1.0]
-            loss_weights = [l / sum(loss_weights) for l in loss_weights]
-            configs_ft["Train"]["loss_weights"] = loss_weights
-        print(f"Training MOE for dataset: {dataset}... with loss weights: {configs_ft['Train']['loss_weights']}")
-        model_checkpoint_state_dict = train(configs_ft)
-        configs_ft["Data"]["train_dataset_limit_per_class"] = None
-        print(f"Finished training MOE for dataset {dataset}")
-        configs_ft["Model"]["moe_ckpt"] = model_checkpoint_state_dict["last_model_path"]
-        configs_ft["Data"]["test_dataset_limit_per_class"] = configs["Data"]["memory_size"] // (task_id + 1)
+        if i > 0:
+            seen_datasets.append(dataset)
+            ft_configs = fill_configs_with_datasets(
+                ft_configs, [dataset], cl_configs["Data"]["real_dataset_name"]
+            )
 
+            if (
+                len(cl_configs["Model"]["ft_ckpt_paths"])
+                == len(cl_configs["Data"]["synthetic_dataset_names"])
+                and cl_configs["Model"]["load_from_ckpt"]
+            ):
+                last_expert_path = cl_configs["Model"]["ft_ckpt_paths"][i]
+            else:
+                print(f"Fine-tuning for dataset: {dataset}...")
+                model_checkpoint_state_dict = train(ft_configs)
+                last_expert_path = model_checkpoint_state_dict["last_model_path"]
+                print(f"Finished fine-tuning for dataset {dataset}")
+            print(f"Last expert path: {last_expert_path}")
+            ft_configs["Model"]["model_type"] = cl_configs["Model"]["model_type"]
+            ft_configs["Train"]["lr"] = cl_configs["Train"]["lr"] * 100
+            ft_configs["Model"]["fine_tune"] = False
+            ft_configs["Model"]["src_ckpts"].append(last_expert_path)
 
-        for j, dataset in enumerate(gan_included_datasets):
+            ft_configs = fill_configs_with_datasets(
+                ft_configs, seen_datasets, cl_configs["Data"]["real_dataset_name"]
+            )
+
+            if cl_configs["Data"]["fixed_memory"]:
+                ft_configs["Train"]["train_dataset_limit_per_class"] = (
+                    cl_configs["Data"]["memory_size"] // 2 // i
+                )
+                ft_configs["Train"]["train_dataset_limit_real"] = (
+                    cl_configs["Data"]["memory_size"] // 2
+                )
+                print(f"Memory size: {cl_configs['Data']['memory_size']}")
+                print(
+                    f"Train dataset limit per class: {ft_configs['Train']['train_dataset_limit_per_class']}"
+                )
+            else:
+                loss_weights = [float(len(seen_datasets)), 1.0]
+                loss_weights = [l / sum(loss_weights) for l in loss_weights]
+                ft_configs["Train"]["loss_weights"] = loss_weights
+            print(
+                f"Training MOE for dataset: {dataset}... with loss weights: {ft_configs['Train']['loss_weights']}"
+            )
+            model_checkpoint_state_dict = train(ft_configs)
+            print(f"Finished training MOE for dataset {dataset}")
+            ft_configs["Train"]["train_dataset_limit_per_class"] = None
+            ft_configs["Train"]["train_dataset_limit_real"] = None
+            ft_configs["Train"]["lr"] = cl_configs["Train"]["lr"]
+            ft_configs["Model"]["moe_ckpt"] = model_checkpoint_state_dict[
+                "last_model_path"
+            ]
+            
+            # ft_configs["Model"]["teacher_ckpt"] = model_checkpoint_state_dict[
+            #     "last_model_path"
+            # ]
+            # ft_configs["Model"]["transformer_ckpt"] = model_checkpoint_state_dict[
+            #     "last_model_path"
+            # ]
+
+        for dataset in cl_configs["Data"]["synthetic_dataset_names"]:
             print(f"Testing MOE for dataset: {dataset}...")
-            configs_ft = fill_configs_with_datasets(configs_ft, [dataset], configs["Data"]["real_dataset_name"])
-            results = test(configs_ft)
-            print(f"Finished testing MOE for dataset {dataset}")
-            print(f"Results for dataset {dataset}: {results}")
-            acc_matrix[i].append(results["acc"])
-            auc_matrix[i].append(results["auc"])
+            ft_configs = fill_configs_with_datasets(
+                ft_configs, [dataset], cl_configs["Data"]["real_dataset_name"]
+            )
+            results = test(ft_configs)
+            num = round(results["acc"], 4)
+            acc_matrix[i].append(round(results["acc"], 4))
+            auc_matrix[i].append(round(results["auc"], 4))
 
-        print(f"Accuracy matrix: {acc_matrix}")
-        print(f"AUC matrix: {auc_matrix}")
-        for j, (acc_row, auc_row) in enumerate(zip(acc_matrix, auc_matrix)):
-            print(f"Accuracy row avg for task {j + 1}: {sum(acc_row) / len(acc_row)}") 
-            print(f"AUC row avg for task {j + 1}: {sum(auc_row) / len(auc_row)}")
-
-
-
-        
+        print("ACC Matrix:")
+        for row in acc_matrix:
+            print(row)
+        print("AUC Matrix:")
+        for row in auc_matrix:
+            print(row)
+        print("ACC Averages:")
+        for j, acc_row in enumerate(acc_matrix):
+            avg = sum(acc_row[:j + 1]) / len(acc_row[:j + 1])
+            print(f"{round(avg, 4)}")
+        print("AUC Averages:")
+        for j, auc_row in enumerate(auc_matrix):
+            avg = sum(auc_row[:j + 1]) / len(auc_row[:j + 1])
+            print(f"{round(avg, 4)}")
