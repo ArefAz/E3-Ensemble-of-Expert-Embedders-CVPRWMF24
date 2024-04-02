9d62459a5b0627ec54a0e5f7b3c98d942f8fe633
Aref Azizpour <aref.azizpour@gmail.com>
2024-04-02 17:17:31 -0400

    eval metric update

---
+++ eval/metrics.py
@@ -0,0 +1,18 @@
+import torch
+import numpy as np
+
+def forward_transfer(results, random_results):
+    n_tasks = len(results)
+    li = []
+    for i in range(1, n_tasks):
+        li.append(results[i - 1][i] - random_results[i])
+
+    return np.mean(li)
+
+def backward_transfer(results):
+    n_tasks = len(results)
+    li = []
+    for i in range(n_tasks - 1):
+        li.append(results[-1][i] - results[i][i])
+
+    return np.mean(li)
